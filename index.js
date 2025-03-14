eabc2351348c8487e35024fce8a2e2b662146913
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2025-03-14 16:18:19 +0500

    commitport

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 
